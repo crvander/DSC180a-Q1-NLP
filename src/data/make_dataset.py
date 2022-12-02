e4b269079c18c12fcae83f24e8250abb3295dfc6
TIMHX <774345882@qq.com>
2022-12-01 16:52:25 -0800

    logging and io formatting

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -5,6 +5,7 @@ from sklearn.model_selection import train_test_split
 import logging
 
 def generate_data(expand = 'True'):
+    logging.info('downloading datasets....')
     # subprocess.run('~/.local/bin/kaggle datasets download -p /home/xic023/DSC180A-Methodology-5/data/raw ankurzing/sentiment-analysis-for-financial-news', shell = True, stdout = subprocess.PIPE)
     # subprocess.run('~/.local/bin/kaggle datasets download -p /home/xic023/DSC180A-Methodology-5/data/raw yash612/stockmarket-sentiment-dataset', shell = True, stdout = subprocess.PIPE)
     # subprocess.run('unzip /home/xic023/DSC180A-Methodology-5/data/raw/\*.zip -d /home/xic023/DSC180A-Methodology-5/data/raw', shell = True, stdout = subprocess.PIPE)
