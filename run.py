e79a214dc1ca4102e8366d59d7dd83301d5f7195
xihong <774345882@qq.com>
2022-12-05 21:57:13 +0000

    more requirements and api for input argument when run

--- run.py
+++ run.py
@@ -12,29 +12,45 @@ import logging
 def main(args):
     logging.basicConfig(filename='myapp.log', level=logging.INFO)
     logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
+    if len(args > 0)
+        data_generate = args[0]
+        training = args[1]
+        test_target = args[2]
+    else:
+        data_generate = ''
+        training = ''
+        test_target = ''
+    logging.info(data_generate, training, testing)
+    
     logging.info('loading data-params...')
     with open('config/data-params.yml', 'r') as file: # All config will be read in module files
         data_config = Box(yaml.full_load(file))
     logging.info(data_config) # here only for logging
     
-    download_data()
-    df = generate_data()
-    save_data(df)
-
-    logging.info('loading training-params...')
-    with open('config/train-params.yml', 'r') as file:
-        train_config = Box(yaml.full_load(file))
-    logging.info(train_config)
-     
-    start = time.time()
-    trainer = train()
-    end = time.time()
-    logging.info('training time: ' + str(end - start))
+    if data_generate = 'generate_data':
+        download_data()
+        df = generate_data()
+        save_data(df)
     
-    logging.info('test start...')
-    test(test_lines = 50)
+    if training = 'train'
+        logging.info('loading training-params...')
+        with open('config/train-params.yml', 'r') as file:
+            train_config = Box(yaml.full_load(file))
+        logging.info(train_config)
+
+        start = time.time()
+        trainer = train()
+        end = time.time()
+        logging.info('training time: ' + str(end - start))
+    
+    if test_target = 'test':
+        logging.info('test start...')
+        test(test_target = 'test', test_lines = 50)
+    else:
+        logging.info('test run start...')
+        test(test_target = 'testdata', test_lines = 3)
     return
 
 
 if __name__ == '__main__':
-    main(sys.argv[1:])
+    main(sys.argv[1:]) # command should be "python3 run.py generate_data train test, for testrun no args needed
--- src/test.py
+++ src/test.py
@@ -12,13 +12,19 @@ with open('config/test-params.yml', 'r') as file:
     test_config = Box(yaml.full_load(file))
 model_path = test_config.model_path
 model_name = test_config.model_name
-input_path = test_config.input_path
+test_path = test_config.test_path
+testdata_path = test_config.testdata_path
 output_dir = test_config.output_dir
 preds_name = test_config.preds_name
 preds_detail_name = test_config.preds_detail_name
 
-def test(test_lines):
+def test(test_target = 'testdata', test_lines = 3):
     out = []
+    if test_target == 'testdata':
+        input_path = testdata_path
+    if test_target == 'test':
+        input_path = test_path
+        
     model = AutoModelForSequenceClassification.from_pretrained(model_path, num_labels = 3)
     tokenizer = AutoTokenizer.from_pretrained('{}/{}'.format(model_path, model_name))
     logging.info('initiate testing from {}/{} ...'.format(model_path, model_name))
